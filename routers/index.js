d5417dae80fa18e4d0b35ca97d7f6ab215619788
E Lim <ellimdl@gmail.com>
2023-02-23 10:14:58 +0100

    refactor code to enable mongoose on cyclic

--- routers/index.js
+++ routers/index.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5001
-// const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// }
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 const {
   create: createProduct,
@@ -34,13 +36,13 @@ app.put("/products/:id", updateOneProduct);
 app.delete("/products/:id", deleteOneProduct);
 
 // Start server
-app.listen(5001, () => {
-  console.log("Listening for requests...");
-});
-
-// connectDB().then(() => {
-//   console.log("db connected");
-//   app.listen(process.env.PORT, () => {
-//     console.log("Listening for requests...");
-//   })
+// app.listen(5001, () => {
+//   console.log("Listening for requests...");
 // });
+
+connectDB().then(() => {
+  console.log("db connected");
+  app.listen(port, () => {
+    console.log("Listening for requests...");
+  })
+});
