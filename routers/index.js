3255bef1042a25c282dfd8e760d0a8b0bea96dc8
E Lim <ellimdl@gmail.com>
2023-02-24 19:57:10 +0800

    correct typo for mongo url

--- routers/index.js
+++ routers/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5001
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
